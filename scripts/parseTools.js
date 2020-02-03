c2d1027c01bc08893a2a407e99aa605d2a2a31be
Dimitris Raptis <dimitrisraptis96@gmail.com>
2020-02-03 10:00:06 +0200

    :construction: Splitting md file

--- scripts/parseTools.js
+++ scripts/parseTools.js
@@ -1,5 +1,14 @@
 const { readFile, readdir, writeFile } = require("fs");
 
 readFile("./README.md", "utf8", (err, data) => {
-  console.log(data);
+  var categories = data.split("### ");
+
+  categories.forEach(category => {
+    const tools = category.split("- **[");
+
+    tools.forEach(tool => {
+      const toolRefined = category.split("\n");
+      console.log(toolRefined);
+    });
+  });
 });
