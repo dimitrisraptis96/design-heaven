88bf295ffd65f00c3bc926c6f021fc3d6c2b4b16
Dimitris Raptis <dimitrisraptis96@gmail.com>
2020-02-04 19:30:23 +0200

    :sparkles: Parsing all the tools successfully

--- scripts/parseTools.js
+++ scripts/parseTools.js
@@ -1,14 +1,60 @@
 const { readFile, readdir, writeFile } = require("fs");
+const { toSlug } = require("./slug");
+const path = require("path");
 
-readFile("./README.md", "utf8", (err, data) => {
-  var categories = data.split("### ");
+const fullLinkOnlyRegex = /^\[([\w\s\d]+)\]\((https?:\/\/[\w\d./?=#]+)\)$/;
 
-  categories.forEach(category => {
-    const tools = category.split("- **[");
+const regexMdLinks = /\[([^\[]+)\](\(.\))/gm;
+const regexMdLinks2 = /\[(.+)\]\(([^ ]+)(?: "(.+)")?\)/;
 
-    tools.forEach(tool => {
-      const toolRefined = category.split("\n");
-      console.log(toolRefined);
-    });
+readFile("./README.md", "utf8", async (err, data) => {
+  const mainContent = data.split("---")[1];
+
+  var categories = mainContent.split("### ");
+  categories.shift();
+  var finalTools = [];
+
+  for (var i = 0; i < categories.length; i++) {
+    const category = categories[i];
+    const title = category.split("\n\n")[0];
+
+    const categoryNew = {
+      name: title,
+      id: title.toLowerCase().replace(" ", "-")
+    };
+
+    const tools = category.split("\n\n")[1].split("\n");
+
+    for (var j = 0; j < tools.length; j++) {
+      const tool = tools[j];
+
+      const description = tool.split("** - ")[1];
+
+      // console.log(tool.match(regexMdLinks2));
+
+      const link = tool.match(regexMdLinks2);
+      console.log(link);
+      const name = link[1];
+      const url = link[2];
+
+      // const [tag, name, url] = link.match(fullLinkOnlyRegex);
+
+      finalTools.push({
+        name,
+        description,
+        url,
+        category: categoryNew.id
+      });
+    }
+  }
+
+  const helpersDir = path.resolve(__dirname, "..", "data");
+
+  const filePath = path.join(helpersDir, `tools.json`);
+  const toolsData = JSON.stringify(finalTools, null, 2) + "\n";
+  console.log(finalTools);
+
+  await writeFile(filePath, toolsData, (err, result) => {
+    if (err) console.log("error", err);
   });
 });
